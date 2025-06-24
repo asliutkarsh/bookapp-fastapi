f01288927c4115281a9fa11add98b715de092212
asliutkarsh <utkarshjaiswal2580@gmail.com>
2025-06-24 13:20:47 +0530

    basic api

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {
+        'message': 'Hello World'
+    }
+
+
 def main():
     print("Hello from bookapp-fastapi!")
+    uvicorn.run(app, host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":
