b97d3955657e2189b5693ccaa0c9d8f52853da54
asliutkarsh <utkarshjaiswal2580@gmail.com>
2025-06-24 13:49:57 +0530

    Add API endpoints for personalized greetings and payload handling

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from fastapi import FastAPI, Header
+from typing import Optional
+from pydantic import BaseModel, Field
 import uvicorn
 
 app = FastAPI()
@@ -7,6 +10,32 @@ app = FastAPI()
 async def root():
     return {
         'message': 'Hello World'
+    }
+    
+@app.get('/{name}')
+async def root_hello(name:str) -> dict:
+    return {
+        'message': f"Hello {name}"
+    }
+
+@app.get('/name')
+async def root_hello_name(name:str, age:Optional[int] = 5) -> dict:
+    return {
+        'message': f"Hello {name}, You age is {age}"
+    }
+    
+class Testpayload(BaseModel):
+    id: str = Field(default_factory=lambda: uuid4().hex)
+    name: str = Field(...,min_length=5, max_length=50)
+    age: Optional[int] = 5
+    
+@app.post('/name')
+async def root_hello_name(payload: Testpayload, 
+                            custom: Optional[str] = Header(None, alias="Hello-Header")  # Read custom header here
+                          ) -> dict:
+    return {
+        'message': f"Hello {payload.name}, You age is {payload.age}, and id {payload.id}",
+        'custom_header': custom
     }
 
 
